6bdfd32b3ea43827de9fa5603d01fa88ee0a3982
Dérick <6derick6@gmail.com>
2024-02-14 21:27:02 -0300

    Trilha React Native: Aula 02 - NLW Expert - 34:33

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,31 @@
 import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
-import { View, Text, FlatList } from "react-native"
-import { CATEGORIES } from "@/utils/data/products"
-import { useState } from "react"
+import { View, Text, FlatList, SectionList } from "react-native"
+import { CATEGORIES, MENU } from "@/utils/data/products"
+import { useState, useRef } from "react"
+import { Product } from "@/components/product"
+import { Link } from "expo-router"
+
 
 export default function Home(){
 
   const [category, setCategory] = useState(CATEGORIES[0])
 
+  const sectionListRef = useRef<SectionList>(null)
+
   function handleCategorySelect(selectedCategory: string){
     setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory)
+
+      if(sectionListRef.current){
+        sectionListRef.current.scrollToLocation({
+          animated: true,
+          sectionIndex,
+          itemIndex: 0,
+        })
+      }
   }
 
   return (
@@ -30,6 +46,25 @@ export default function Home(){
         className="max-h-10 mt-5"
         contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
         showsHorizontalScrollIndicator={false}
+      />
+
+      <SectionList 
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/products/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => 
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   )
